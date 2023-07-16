80d283c5f5bb468be2546dd8403093398a94f1d6
lamissamara <92857502+lamissamara@users.noreply.github.com>
2023-07-16 15:01:21 +0300

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,22 @@
+﻿
+
+new Vue({
+      el: '#app',
+      data() {
+        return {
+          items: [],
+          newItem: ''
+        }
+      },
+      methods: {
+        addItem() {
+          if (this.newItem !== '') {
+            this.items.push(this.newItem);
+            this.newItem = '';
+          }
+        },
+        deleteItem(index) {
+          this.items.splice(index, 1);
+        }
+      }
+    });
